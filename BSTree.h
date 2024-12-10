7d1a1b0e5f392d543a8eb3b05cfd526d6e6728ee
root <root@HPNuria.>
2024-12-10 19:42:02 +0100

    Examen

--- BSTree.h
+++ BSTree.h
@@ -133,6 +133,46 @@ public:
     void remove(T e) {
         root = remove(root, e);
     }
+
+// EXAMEN
+    
+    T min(BSNode<T> *n) const { // Devuelve el valor mínimo
+    	if (n == nullptr) {
+        	throw std::runtime_error("El nodo está vacío");
+    	} else if (n->left != nullptr) { // Recursividad de min
+        	return min(n->left);
+    	} else { // Al ser el valor mínimo lo devuelve
+        	return n->elem;
+    	}
+    }
+
+    T max() const { // Obtiene el valor máximo
+        if (root == nullptr) { 
+            throw std::runtime_error("El árbol está vacío");
+        }
+        return max(root); // Devuelve el máximo del ABB
+    }
+
+    T min() const { // Obtiene el valor mínimo
+    	if (root == nullptr) {
+        	throw std::runtime_error("El árbol está vacío");
+    	}
+    	return min(root); // Devuelve el mínimo del ABB
+    }
+    
+    void prune() { //Elimina los valores máximo y mínimo del árbol
+    	if (root == nullptr) {
+        	throw std::runtime_error("El árbol está vacío");
+    	}
+	
+	// Almacenamos los valores máximos y mínimos
+    	T val_min = min();
+    	T val_max = max();
+
+	// Borramos los valores obtenidos
+    	remove(val_min);
+    	remove(val_max);
+    }    
 };
 
 #endif // BSTREE_H
